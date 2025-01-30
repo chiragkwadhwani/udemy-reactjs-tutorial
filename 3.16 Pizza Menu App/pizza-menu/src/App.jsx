a26b16532a4e2e1c2e5be7adae3809ee7571a7ad
Chirag <chirag_wadhwani289@hotmail.com>
2025-01-30 15:17:38 +0700

    pizza menu part 2

--- 3.16 Pizza Menu App/pizza-menu/src/App.jsx
+++ 3.16 Pizza Menu App/pizza-menu/src/App.jsx
@@ -1,48 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "/src/pizzapage.css";
+import "./pizzapage.css";
 
 const pizzaData = [
   {
     name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
-    photoName: "pizzas/focaccia.jpg",
+    photoName: "/src/assets/focaccia.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Margherita",
     ingredients: "Tomato and mozarella",
     price: 10,
-    photoName: "pizzas/margherita.jpg",
+    photoName: "/src/assets/margherita.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Spinaci",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
-    photoName: "pizzas/spinaci.jpg",
+    photoName: "/src/assets/spinaci.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Funghi",
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
-    photoName: "pizzas/funghi.jpg",
+    photoName: "/src/assets/funghi.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Salamino",
     ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
-    photoName: "pizzas/salamino.jpg",
+    photoName: "/src/assets/salamino.jpg",
     soldOut: true,
   },
   {
     name: "Pizza Prosciutto",
     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
-    photoName: "pizzas/prosciutto.jpg",
+    photoName: "/src/assets/prosciutto.jpg",
     soldOut: false,
   },
 ];
@@ -70,10 +70,29 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (<>
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza 
+
+      {numPizzas > 0 ? (
+        <>
+          <p>Authentic Italian cuisine. 6 creative dishes to choose from. All
+          from our stone oven, all organic, all delicious.</p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza 
+              pizzaObj={pizza}
+              key={pizza.name}
+              />
+            ))}
+          </ul>
+        </>
+      ) : <p>We're still working on our menu. Please come back later :)</p>}
+
+      {/* <Pizza 
         name="Pizza Spinaci" 
         ingredients="Tomato, mozarella, spinach, and ricotta cheese" 
         photoName="src/assets/spinaci.jpg" 
@@ -85,22 +104,24 @@ function Menu() {
         ingredients="Tomato, mushrooms" 
         photoName="src/assets/funghi.jpg" 
         price={12}
-      />
+      /> */}
     </main>
   </>);
 }
 
-function Pizza(props) {
-  console.log(props);
+function Pizza({pizzaObj}) {
+  // if (pizzaObj.soldOut) 
+  //   return null;
+
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   )
 }
 
@@ -117,7 +138,24 @@ function Footer() {
   //   alert("Sorry we're currently closed");
   // }
 
-  return <footer className="footer">{new Date().toLocaleTimeString()}. We're currently open</footer>
+  return ( 
+    <footer className="footer">
+      {isOpen ? (
+        <Order openHour={openHour} closeHour={closeHour}/>
+      ) : (
+        <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>
+      )}
+    </footer>
+  );
+}
+
+function Order({openHour, closeHour}) {
+  return (
+    <div className="order">
+    <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+    <button className="btn">Order</button>
+    </div>
+  )
 }
 
 export default App;
