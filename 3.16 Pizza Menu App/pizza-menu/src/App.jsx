9985bbb6621daf77a0e8b0dd08d19c35d6573ab2
Chirag <chirag_wadhwani289@hotmail.com>
2025-01-30 13:30:42 +0700

    continue the pizza menu page part 1

--- 3.16 Pizza Menu App/pizza-menu/src/App.jsx
+++ 3.16 Pizza Menu App/pizza-menu/src/App.jsx
@@ -1,35 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "/src/pizzapage.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      {/* <h1>Hello React!</h1> */}
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
   )
 }
 
-export default App
+function Header() {
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  )
+}
+
+function Menu() {
+  return (<>
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza 
+        name="Pizza Spinaci" 
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese" 
+        photoName="src/assets/spinaci.jpg" 
+        price={10}
+      />
+
+      <Pizza 
+        name="Pizza Funghi" 
+        ingredients="Tomato, mushrooms" 
+        photoName="src/assets/funghi.jpg" 
+        price={12}
+      />
+    </main>
+  </>);
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
+  )
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour < closeHour;
+
+  // if (isOpen) {
+  //   alert("We're currently open");
+  // }
+  // else {
+  //   alert("Sorry we're currently closed");
+  // }
+
+  return <footer className="footer">{new Date().toLocaleTimeString()}. We're currently open</footer>
+}
+
+export default App;
