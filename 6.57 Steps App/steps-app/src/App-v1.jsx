b635972811d7878a1003a17252a1e9c4afaf8bab
Chirag <chirag_wadhwani289@hotmail.com>
2025-02-03 12:26:18 +0700

    reusable-button for steps-app with children prop

--- 6.57 Steps App/steps-app/src/App-v1.jsx
+++ 6.57 Steps App/steps-app/src/App-v1.jsx
@@ -48,19 +48,11 @@ export default function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          {/* <p className="message">Step {step}: {messages[step - 1]}</p> */}
-          <StepMessage step={step}>{messages[step - 1]}
-
-          <div class="buttons">
-            <Button textColor="#333" bgColor="#e7e7e7" onClick={() => alert(`Learn how to ${messages[step - 1]}`)}>Learn How</Button>
-          </div>
-          </StepMessage>
+          <p className="message">Step {step}: {messages[step - 1]}</p>
 
           <div className="buttons">
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}><span>👈</span> Previous</Button>
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>Next <span>👉</span></Button>
-            {/* <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handlePrevious}>Previous</button> */}
-            {/* <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handleNext}>Next</button> */}
+            <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handlePrevious}>Previous</button>
+            <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handleNext}>Next</button>
           </div>
         </div>
       )}
@@ -103,14 +95,4 @@ export default function App() {
     </div>
     </div>
   );
-}
-
-function StepMessage({step, children}) {
-  return <div className="message"><h3>Step {step}</h3>{children}</div>
-}
-
-function Button({textColor, bgColor, onClick, children}) {
-  return <button style={{backgroundColor: bgColor, color: textColor}} onClick={onClick}>
-    {children}
-  </button>;
 }
