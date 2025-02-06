ad57fda330c7588b01d0cd26ce2ae9e748d6f2a1
Chirag <chirag_wadhwani289@hotmail.com>
2025-02-06 13:36:52 +0700

    section 10 usepopcorn completed

--- 10.106 UsePopcorn app/usepopcorn/src/main.jsx
+++ 10.106 UsePopcorn app/usepopcorn/src/main.jsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
 import StarRating from './StarRating.jsx'
+import { useState } from 'react';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating}/>
+      <h1>This movie was rated {movieRating}</h1>
+    </div>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,5 +22,7 @@ createRoot(document.getElementById('root')).render(
     <StarRating maxRating={5}/>
     <StarRating maxRating={10}/>
     <StarRating />
+    <StarRating size={24} color="red" messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} defaultRating={3}/>
+    <Test />
   </StrictMode>,
 )
