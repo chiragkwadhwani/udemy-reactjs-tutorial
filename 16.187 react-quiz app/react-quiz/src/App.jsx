822aa61c71bb30a1e4ff931d5c6236eb4539ece0
Chirag <chirag_wadhwani289@hotmail.com>
2025-06-25 13:18:54 +0700

    init react-quiz app

---
+++ 16.187 react-quiz app/react-quiz/src/App.jsx
@@ -0,0 +1,10 @@
+import DateCounter from "./DateCounter";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <h1>React Quiz</h1> */}
+      <DateCounter />
+    </div>
+  );
+}
