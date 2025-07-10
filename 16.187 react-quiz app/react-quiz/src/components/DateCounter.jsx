7fa4245f0441c140d367fda97b3816dee34e73d1
Chirag <chirag_wadhwani289@hotmail.com>
2025-07-10 16:49:48 +0700

    worldwise app - section 17 only

--- 16.187 react-quiz app/react-quiz/src/components/DateCounter.jsx
+++ 16.187 react-quiz app/react-quiz/src/components/DateCounter.jsx
@@ -3,7 +3,6 @@ import { useReducer } from "react";
 const initialState = { ...state, count: 0, step: 1 };
 
 function reducer(state, action) {
-  console.log(state, action);
 //   if (action.type === 'dec') return state + action.payload;
 //   if (action.type === 'inc') return state + action.payload;
 //   if (action.type === 'setCount') return action.payload;
---
+++ 17.204 WorldWise App/worldwise/src/components/CountryList.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import CountryItem from "./CountryItem";
+// import PropTypes from 'prop-types';
+import Message from "./Message";
+
+function CountryList({cities, isLoading}) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+// CountryList.propTypes = {
+//   cities: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       name: PropTypes.string.isRequired,
+//       country: PropTypes.string.isRequired,
+//       population: PropTypes.number.isRequired,
+//     })
+//   ).isRequired,
+//   isLoading: PropTypes.bool.isRequired,
+// };
+
+export default CountryList;
